c20a00f446604ebc0aa3afd2a82285882ecadcb7
johnsawickifairfieldu <32599993+johnsawickifairfieldu@users.noreply.github.com>
2017-11-05 20:31:50 -0500

    Commit 2
    We will have to keep two databases with login stuff separate. Added User model and controllers. Modified home page and shared layout. Will have to figure out a way to sync user data between both models/databases.

--- IslandEscape/IslandEscape/Models/IdentityModels.cs
+++ IslandEscape/IslandEscape/Models/IdentityModels.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -16,6 +15,7 @@ namespace IslandEscape.Models
             // Add custom user claims here
             return userIdentity;
         }
+               
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
---
+++ IslandEscape/IslandEscape/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace IslandEscape.Models
+{
+    public class User
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int AccessLevel { get; set; }
+
+    }
+
+    public class IslandEscapeDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
